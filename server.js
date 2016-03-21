631e031b0439a17cd6982f8c64b09799e959738b
Hien Nguyen <hien.nguyen@felizz.com>
2016-03-21 12:05:57 +0700

    Added comment

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var http = require('http');
 var favicon = require('serve-favicon');
-var nconf = require('nconf');
 var path = require('path');
 var morgan = require('morgan');
-var routes = require('./routes/home');
 var winston = require('winston');
 var bodyParser = require('body-parser');
 var redis = require('redis');
 var util = require('util');
 
+var routes = require('./routes/home');
+
+var nconf = require('nconf');
 // read in keys and secrets.  You can store these in a variety of ways.  I like to use a keys.json 
 // file that is in the .gitignore file, but you can also store them in the env
 nconf.argv().env().file('keys.json');
 
-var universe = "universe";
+var UNIVERSE = "universe";
 
 // set up a single instance of a winston logger
 var logger = new (winston.Logger)({
@@ -24,6 +25,10 @@ var logger = new (winston.Logger)({
 });
 logger.info(`Started wazstagram running on ${process.title} ${process.version}`);
 
+/**
+ *
+ * @returns Redis client for node.js. Server is defined as (port=6379, host=got from config file).
+ */
 function createRedisClient() {
     return redis.createClient(
         6379,
@@ -37,7 +42,13 @@ function createRedisClient() {
     });    
 }
 
-// create redis clients for the publisher and the subscriber
+/**
+ * Create redis clients for the publisher and the subscriber.
+ * The working model is:
+ *          Instagram POST -> Redis Publisher (cache + publish events to topic) -> Redis Subscriber (public event to socket.io client)
+ *
+ * When client join the first time, Cache of Redis Publisher is read and sent to client.
+ */
 var redisSubClient = createRedisClient();
 var redisPubClient = createRedisClient();
 
@@ -52,10 +63,13 @@ app.use(morgan('dev'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+
 app.use(function(req, res, next) {
+
+    //Assign local variable for each request-response.
     res.locals.nconf = nconf;
     res.locals.logger = logger;
-    res.locals.publishFunc = publishImage;
+    res.locals.publishFunc = publishImage;  //will be called when Instagram send a POST message.
     next();
 });
 app.use('/', routes)
@@ -100,6 +114,10 @@ server.listen(app.get('port'), function(){
 io.sockets.on('connection', function (socket) {
     socket.on('setCity', function (data) {
         logger.info('new connection: ' + data.city);
+
+        /**
+         * Initially send 100 pictures to client.
+         */
         redisPubClient.lrange(data.city, 0, 100, function(err, picCache) {
             if (err) {
                 logger.error(err);
@@ -107,19 +125,27 @@ io.sockets.on('connection', function (socket) {
             }
             logger.info("cache length: " + picCache.length);
             for (var i = picCache.length-1; i >= 0; i--) {
+
+                //Send a single picture to client.
                 socket.emit('newPic', picCache[i].toString());
             }
+
+            /**
+             * Join a room to receive pictures in real-time.
+             */
             socket.join(data.city);
         });
     });
 });
 
-// listen to new images from redis pub/sub
+/**
+ * Listen to new images from redis pub/sub
+ */
 redisSubClient.on('message', function(channel, message) {
     logger.verbose('channel: ' + channel + " ; message: " + message);
     var m = JSON.parse(message.toString());
     io.sockets.in (m.city).emit('newPic', m.pic);
-    io.sockets.in (universe).emit('newPic', m.pic);
+    io.sockets.in (UNIVERSE).emit('newPic', m.pic);
 }).subscribe('pics');
 
 // send an event to redis letting all clients know there
@@ -132,8 +158,8 @@ function publishImage(message) {
     // cache results to ensure users get an initial blast of (n) images per city
     redisPubClient.lpush(message.city, message.pic);
     redisPubClient.ltrim(message.city, 0, 100);
-    redisPubClient.lpush(universe, message.pic);
-    redisPubClient.ltrim(universe, 0, 100);
+    redisPubClient.lpush(UNIVERSE, message.pic);
+    redisPubClient.ltrim(UNIVERSE, 0, 100);
 }
 
 module.exports = app;
